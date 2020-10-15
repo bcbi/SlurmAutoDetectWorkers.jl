25bbaa49875dba3fc897627e6cfd2f2434f5b2c4
Dilum Aluthge <dilum@aluthge.com>
2020-10-15 02:22:48 -0400

    Modify a test (#4)

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,5 @@ using SlurmAutoDetectWorkers
 using Test
 
 @testset "SlurmAutoDetectWorkers.jl" begin
-    @test SlurmAutoDetectWorkers._foo(1) == 2
+    @test SlurmAutoDetectWorkers._foo(2) == 4
 end
